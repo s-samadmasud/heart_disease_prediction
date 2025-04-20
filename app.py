6e0a168fe4f3bb2f7552ce7f2a2dfd22baa6c570
suhal samad <samadsuhal@gmail.com>
2025-04-20 11:37:38 +0600

    files edited

--- app.py
+++ app.py
@@ -70,5 +70,6 @@ def contact():
 @app.route('/blog')
 def blog():
     return render_template("blog.html")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
